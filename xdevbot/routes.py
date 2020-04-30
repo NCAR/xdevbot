3d36b06721475399f332c24196834335f1913a77
Kevin Paul <kpaul@ucar.edu>
2020-04-30 11:22:46 -0600

    New templates and formatting

--- xdevbot/routes.py
+++ xdevbot/routes.py
@@ -1,12 +1,13 @@
 import pathlib
 
-from .views import index
+from .views import configure, landing
 
 PROJECT_ROOT = pathlib.Path(__file__).parent
 
 
 def setup_routes(app):
-    app.router.add_get('/', index)
+    app.router.add_get('/', landing)
+    app.router.add_get('/configure', configure)
     setup_static_routes(app)
 
 
--- xdevbot/views.py
+++ xdevbot/views.py
@@ -1,6 +1,11 @@
 import aiohttp_jinja2
 
 
-@aiohttp_jinja2.template('index.html.j2')
-async def index(request):
-    return {}
+@aiohttp_jinja2.template('landing.html.j2')
+async def landing(request):
+    return {'title': 'Xdevbot'}
+
+
+@aiohttp_jinja2.template('configure.html.j2')
+async def configure(request):
+    return {'title': 'Xdevbot'}
