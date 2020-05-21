5db460fed8c3d07b687fc0ca4ab081388e2d6c54
Kevin Paul <kpaul@ucar.edu>
2020-05-21 13:43:12 -0600

    Add new tests for views

--- xdevbot/tests/test_views.py
+++ xdevbot/tests/test_views.py
@@ -20,17 +20,34 @@ async def test_index(aiohttp_client, loop):
     assert soup.title.string == '404 Page Not Found'
 
 
-async def test_github(aiohttp_client, loop):
+async def test_ghmain(aiohttp_client, loop):
     app = await init_app()
     client = await aiohttp_client(app)
 
-    resp = await client.get('/github')
+    resp = await client.get('/ghmain')
     assert resp.status == 200
     html_doc = await resp.text()
     soup = BeautifulSoup(html_doc, 'html.parser')
     assert soup.title.string == '404 Page Not Found'
 
-    resp = await client.post('/github', data={})
+    resp = await client.post('/ghmain', data={})
+    assert resp.status == 200
+    html_doc = await resp.text()
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    assert soup.text == 'OK'
+
+
+async def test_ghwatch(aiohttp_client, loop):
+    app = await init_app()
+    client = await aiohttp_client(app)
+
+    resp = await client.get('/ghwatch')
+    assert resp.status == 200
+    html_doc = await resp.text()
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    assert soup.title.string == '404 Page Not Found'
+
+    resp = await client.post('/ghwatch', data={})
     assert resp.status == 200
     html_doc = await resp.text()
     soup = BeautifulSoup(html_doc, 'html.parser')
