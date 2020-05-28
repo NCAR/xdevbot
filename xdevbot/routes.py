69e57be67fc0f31d2c0772323daf5ff32c058a97
Kevin Paul <kpaul@ucar.edu>
2020-05-28 11:26:53 -0600

    Modify post routes

--- xdevbot/routes.py
+++ xdevbot/routes.py
@@ -7,8 +7,8 @@ PROJECT_ROOT = pathlib.Path(__file__).parent
 
 def setup_routes(app):
     app.router.add_get('/', home)
-    app.router.add_post('/ghmain', ghmain)
-    app.router.add_post('/ghwatch', ghwatch)
+    app.router.add_post('/gh/main', ghmain)
+    app.router.add_post('/gh/watch', ghwatch)
     setup_static_routes(app)
 
 
