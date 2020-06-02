c572bfdc814573e548064a3fec68798664cf7577
Kevin Paul <kpaul@ucar.edu>
2020-06-02 17:08:13 -0600

    Adding script to set up main project board

--- xdevbot/project.py
+++ xdevbot/project.py
@@ -8,80 +8,19 @@ import aiohttp
 
 logging.basicConfig(level=logging.INFO)
 
+MAIN_REPO_URL = 'https://api.github.com/repos/NCAR/xdevbot-testing'
 XDEVBOT_MAIN_ENDPOINT = 'http://xdevbot.herokuapp.com/gh/main'
-XDEVBOT_WATCH_ENDPOINT = 'http://xdevbot.herokuapp.com/gh/watch'
 
 
-class UnauthorizedException(Exception):
-    """Unauthorized user exception"""
-
-
-async def accept_invitations(
-    url='https://api.github.com/user/repository_invitations',
-    username=os.environ.get('GITHUB_USER', ''),
-    token=os.environ.get('GITHUB_TOKEN', ''),
-):
-
-    headers = {
-        'Accept': 'application/vnd.github.v3+json',
-        'Authorization': f'token {token}',
-        'User-Agent': username,
-    }
-
-    async with aiohttp.ClientSession(headers=headers) as client:
-
-        logging.info('Retrieving open invitations.')
-        async with client.get(url) as response:
-            invitations = await response.json()
-            if response.status != 200:
-                logging.error('Failed to retrieve invitations.')
-                return
-
-        if len(invitations) > 0:
-            msg = f'Accepting {len(invitations)} open invitations.'
-        else:
-            msg = 'No open invitations found.'
-        logging.info(msg)
-
-        for invitation in invitations:
-            repo_name = invitation['repository']['full_name']
-            invitation_id = invitation['id']
-            async with client.patch(f'{url}/{invitation_id}') as response:
-                status = await response.status
-                if status == 204:
-                    logging.info(f'Successfully accepted invitation to {repo_name}.')
-                else:
-                    logging.error(f'Failed to accept invitation to {repo_name}.')
-
-
-async def validate_main_repo(
-    url='https://api.github.com/repos/NCAR/xdevbot-testing',
-    username=os.environ.get('GITHUB_USER', None),
-    token=os.environ.get('GITHUB_TOKEN', None),
-):
-
-    main_repo = await get_main_repo(url, username=username, token=token)
-
-    main_hook = await get_main_hook(
-        main_repo['hooks_url'],
-        create=main_repo['permissions']['admin'],
-        username=username,
-        token=token,
-    )
-
-    project_url = f"{main_repo['url']}/projects"
-    main_project = await get_main_project(
-        project_url, create=main_repo['permissions']['admin'], username=username, token=token
-    )
-
-    logging.info('Writing main repository data to file.')
-    db_main = {'repo': main_repo, 'hook': main_hook, 'board': main_project}
-    with open('database.json', 'w') as db:
-        json.dump(db_main, db, indent=3)
+class InvalidRequest(Exception):
+    """Invalid request exception"""
 
 
 async def get_main_repo(
-    url, username=os.environ.get('GITHUB_USER', None), token=os.environ.get('GITHUB_TOKEN', None)
+    url,
+    database={},
+    username=os.environ.get('GITHUB_USER', ''),
+    token=os.environ.get('GITHUB_TOKEN', ''),
 ):
 
     headers = {
@@ -96,14 +35,18 @@ async def get_main_repo(
         async with client.get(url) as response:
             main_repo = await response.json()
 
-    return main_repo
+            if response.status == 200:
+                database['repo'] = main_repo
+            else:
+                logging.error('Could not retrieve main repository metadata.')
 
 
 async def get_main_hook(
     url,
     create=True,
-    username=os.environ.get('GITHUB_USER', None),
-    token=os.environ.get('GITHUB_TOKEN', None),
+    database={},
+    username=os.environ.get('GITHUB_USER', ''),
+    token=os.environ.get('GITHUB_TOKEN', ''),
 ):
 
     headers = {
@@ -117,6 +60,10 @@ async def get_main_hook(
         logging.info('Retrieving main repository webhooks.')
         async with client.get(url) as response:
             hooks = await response.json()
+            if response.status != 200:
+                msg = 'Could not retrieve main repository webhooks'
+                logging.error(msg)
+                raise InvalidRequest(msg)
 
         potl_hooks = []
         for hook in hooks:
@@ -127,12 +74,6 @@ async def get_main_hook(
                 and hook['active']
             ):
                 potl_hooks.append(hook)
-
-        if len(potl_hooks) > 0:
-            msg = f'Found {len(potl_hooks)} potential main repository webhooks.'
-        else:
-            msg = 'No potential main repository webhooks found.'
-        logging.debug(msg)
 
         if len(potl_hooks) == 0:
             if create:
@@ -149,7 +90,7 @@ async def get_main_hook(
             else:
                 msg = 'No main repository webhook found.'
                 logging.error(msg)
-                raise UnauthorizedException(msg)
+                raise InvalidRequest(msg)
         elif len(potl_hooks) == 1:
             logging.info('Existing main repository webhook found.')
             main_hook = potl_hooks[0]
@@ -164,14 +105,15 @@ async def get_main_hook(
             )
             main_hook = potl_hooks[i_hook]
 
-    return main_hook
+    database['hook'] = main_hook
 
 
 async def get_main_project(
     url,
+    database={},
     create=True,
-    username=os.environ.get('GITHUB_USER', None),
-    token=os.environ.get('GITHUB_TOKEN', None),
+    username=os.environ.get('GITHUB_USER', ''),
+    token=os.environ.get('GITHUB_TOKEN', ''),
 ):
 
     project_name = 'Xdevbot Main Project'
@@ -189,6 +131,10 @@ async def get_main_project(
         logging.info('Searching for main project board')
         async with client.get(url) as response:
             projects = await response.json()
+            if response.status != 200:
+                msg = 'Could not find main project board'
+                logging.error(msg)
+                raise InvalidRequest(msg)
 
         potl_projects = []
         for project in projects:
@@ -201,17 +147,25 @@ async def get_main_project(
                 request = dict(name=project_name, body=project_description)
                 async with client.post(url, json=request) as response:
                     main_project = await response.json()
+                    if response.status != 201:
+                        msg = 'Could not create main project board'
+                        logging.error(msg)
+                        raise InvalidRequest(msg)
 
                 url = main_project['columns_url']
                 main_columns = []
                 for column in project_columns:
                     async with client.post(url, json={'name': column}) as response:
                         main_column = await response.json()
+                        if response.status != 201:
+                            msg = f'Could not create main project column {column}'
+                            logging.error(msg)
+                            raise InvalidRequest(msg)
                     main_columns.append(main_column)
             else:
                 msg = 'No main project board found.'
                 logging.error(msg)
-                raise UnauthorizedException(msg)
+                raise InvalidRequest(msg)
         elif len(potl_projects) == 1:
             logging.info('Existing main project board found.')
             main_project = potl_projects[0]
@@ -219,14 +173,38 @@ async def get_main_project(
             url = main_project['columns_url']
             async with client.get(url) as response:
                 main_columns = await response.json()
+                if response.status != 200:
+                    msg = 'Could not get main project columns'
+                    logging.error(msg)
+                    raise InvalidRequest(msg)
         else:
             msg = 'Multiple main project boards found with same name.'
             logging.error(msg)
             raise NotImplementedError(msg)
 
+    database['project'] = main_project
+    database['columns'] = main_columns
+
 
 if __name__ == '__main__':
+    database = {}
+
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(accept_invitations())
-    loop.run_until_complete(validate_main_repo())
+
+    loop.run_until_complete(get_main_repo(MAIN_REPO_URL, database=database))
+
+    hooks_url = database['repo']['hooks_url']
+    projects_url = f"{database['repo']['url']}/projects"
+    create = database['repo']['permissions']['admin']
+
+    tasks = [
+        loop.create_task(get_main_hook(hooks_url, create=create, database=database)),
+        loop.create_task(get_main_project(projects_url, create=create, database=database)),
+    ]
+    loop.run_until_complete(asyncio.gather(*tasks))
+
     loop.close()
+
+    logging.info('Writing main repository data to file.')
+    with open('project_database.json', 'w') as db:
+        json.dump(database, db, indent=3)
