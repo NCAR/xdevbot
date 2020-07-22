df515bb962521c2e73653a47617b008792bac1b6
Kevin Paul <kpaul@ucar.edu>
2020-07-22 15:26:38 -0600

    Update endpoint (and tests)

--- tests/test_app.py
+++ tests/test_app.py
@@ -19,7 +19,7 @@ async def test_404(app, aiohttp_client, loop):
 
 async def test_405(app, aiohttp_client, loop):
     client = await aiohttp_client(app)
-    resp = await client.get('/')
+    resp = await client.get('/hooks/github/')
     assert resp.status == 405
     text = await resp.text()
     assert text == '405: Method Not Allowed'
@@ -41,7 +41,7 @@ async def test_http_exception(app, aiohttp_client, loop):
 async def test_no_route(app, aiohttp_client, loop):
     client = await aiohttp_client(app)
     headers = {'user-agent': 'GitHub-Hookshot/asef3'}
-    resp = await client.post('/', headers=headers, json={})
+    resp = await client.post('/hooks/github/', headers=headers, json={})
     assert resp.status == 501
     text = await resp.text()
     assert text == '501: GitHub route undefined'
@@ -51,7 +51,7 @@ async def test_issues_created(app, aiohttp_client, loop):
     client = await aiohttp_client(app)
     headers = {'user-agent': 'GitHub-Hookshot/asef3', 'X-GitHub-Event': 'issues'}
     data = {'action': 'created'}
-    resp = await client.post('/', headers=headers, json=data)
+    resp = await client.post('/hooks/github/', headers=headers, json=data)
     assert resp.status == 200
     text = await resp.text()
     assert text == 'Thanks!'
@@ -61,7 +61,7 @@ async def test_no_user_agent(app, aiohttp_client, loop):
     client = await aiohttp_client(app)
     headers = {'X-GitHub-Event': 'issues'}
     data = {'action': 'created'}
-    response = await client.post('/', headers=headers, json=data)
+    response = await client.post('/hooks/github/', headers=headers, json=data)
     assert response.status == 406
     text = await response.text()
     assert text == '406: User agent looks incorrect'
@@ -70,7 +70,7 @@ async def test_no_user_agent(app, aiohttp_client, loop):
 async def test_non_json(app, aiohttp_client, loop):
     client = await aiohttp_client(app)
     headers = {'user-agent': 'GitHub-Hookshot/asef3', 'X-GitHub-Event': 'issues'}
-    response = await client.post('/', headers=headers, data=b'bytes')
+    response = await client.post('/hooks/github/', headers=headers, data=b'bytes')
     assert response.status == 415
     text = await response.text()
     assert text == '415: Not a JSON payload'
