916597f616bd20fc87700b2f25bb79285af0ed37
Kevin Paul <kpaul@ucar.edu>
2020-08-04 09:17:53 -0600

    Add token parameter to CLI

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,13 +2,14 @@ import pytest
 
 from xdevbot.cli import NAME, cli
 
-DEFAULT_CONFIG = {'host': None, 'port': None}
+DEFAULT_CONFIG = {'host': None, 'port': None, 'token': None}
 
 
 @pytest.fixture
 def cli_env(monkeypatch):
     monkeypatch.setenv(f'{NAME.upper()}_HOST', 'http://127.0.0.1')
     monkeypatch.setenv(f'{NAME.upper()}_PORT', '6789')
+    monkeypatch.setenv(f'{NAME.upper()}_TOKEN', 'sometoken')
 
 
 def test_cli_defaults():
@@ -30,10 +31,20 @@ def test_cli_port():
     assert ctx.params == params
 
 
+def test_cli_token():
+    params = DEFAULT_CONFIG.copy()
+    params['token'] = 'xtokenx'
+    ctx = cli.make_context(
+        cli.name, args=['--token', str(params['token'])], auto_envvar_prefix=NAME
+    )
+    assert ctx.params == params
+
+
 def test_cli_env(cli_env):
     params = DEFAULT_CONFIG.copy()
     params['host'] = 'http://127.0.0.1'
     params['port'] = 6789
+    params['token'] = 'sometoken'
     ctx = cli.make_context(cli.name, args=[], auto_envvar_prefix=NAME)
     assert ctx.params == params
 
@@ -42,5 +53,6 @@ def test_cli_env_overridden(cli_env):
     params = DEFAULT_CONFIG.copy()
     params['host'] = 'http://127.0.0.1'
     params['port'] = 9999
+    params['token'] = 'sometoken'
     ctx = cli.make_context(cli.name, args=['--port', str(params['port'])], auto_envvar_prefix=NAME)
     assert ctx.params == params
