029e89101110cb58155f988e5911185b14caa9af
Kevin Paul <kpaul@ucar.edu>
2020-08-05 13:51:33 -0600

    Clearing environment before tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -12,26 +12,33 @@ def cli_env(monkeypatch):
     monkeypatch.setenv(f'{NAME.upper()}_TOKEN', 'sometoken')
 
 
-def test_cli_defaults():
+@pytest.fixture
+def no_env(monkeypatch):
+    monkeypatch.delenv(f'{NAME.upper()}_HOST', raising=False)
+    monkeypatch.delenv(f'{NAME.upper()}_PORT', raising=False)
+    monkeypatch.delenv(f'{NAME.upper()}_TOKEN', raising=False)
+
+
+def test_cli_defaults(no_env):
     ctx = cli.make_context(cli.name, args=[], auto_envvar_prefix=NAME)
     assert ctx.params == DEFAULT_CONFIG
 
 
-def test_cli_host():
+def test_cli_host(no_env):
     params = DEFAULT_CONFIG.copy()
     params['host'] = 'http://localhost'
     ctx = cli.make_context(cli.name, args=['--host', params['host']], auto_envvar_prefix=NAME)
     assert ctx.params == params
 
 
-def test_cli_port():
+def test_cli_port(no_env):
     params = DEFAULT_CONFIG.copy()
     params['port'] = 4567
     ctx = cli.make_context(cli.name, args=['--port', str(params['port'])], auto_envvar_prefix=NAME)
     assert ctx.params == params
 
 
-def test_cli_token():
+def test_cli_token(no_env):
     params = DEFAULT_CONFIG.copy()
     params['token'] = 'xtokenx'
     ctx = cli.make_context(
