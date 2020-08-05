125240531ec1618c63a2627613077772bfd339be
Kevin Paul <kpaul@ucar.edu>
2020-08-05 12:23:38 -0600

    Adding logging messages

--- xdevbot/routes.py
+++ xdevbot/routes.py
@@ -1,3 +1,5 @@
+import logging
+
 from aiohttp import web
 
 from xdevbot import github, projects, queries, utils
@@ -10,11 +12,13 @@ CONFIG_URL = 'https://raw.githubusercontent.com/NCAR/xdev/master/xdevbot.yaml'
 async def opened(event: github.EventType):
     ref = event.payload[event.element]['html_url']
     repo = event.payload[event.element]['repository']['full_name']
+    logging.info(f'Received {event.element} {event.action} event: {ref}')
 
     cfg_data = await utils.read_remote_yaml(CONFIG_URL)
     df = projects.build_config_frame(cfg_data)
     project_urls = df[df['repo'] == repo]['project_url'].to_list()
     if len(project_urls) == 0:
+        logging.info(f'No projects associated with repo: {repo}')
         return web.Response()
 
     token = event.app['token']
@@ -24,6 +28,7 @@ async def opened(event: github.EventType):
 
     async with github.ProjectClientSession(token=token) as session:
         for project_url in project_urls:
+            logging.info(f'Creating new card on project: {project_url}')
             column_id = columns[project_url][column_name]
             await session.create_project_card(note=ref, column_id=column_id)
 
@@ -36,12 +41,14 @@ async def opened(event: github.EventType):
 @github.route('pull_request', 'reopened')
 async def closed(event: github.EventType):
     ref = event.payload[event.element]['html_url']
+    logging.info(f'Received {event.element} {event.action} event: {ref}')
 
     token = event.app['token']
     card_data = await github.graphql_query(queries.GET_ALL_CARDS, token=token)
     df = projects.build_cards_frame(card_data)
     cards = df[df['ref'] == ref]
     if len(cards) == 0:
+        logging.info(f'No cards found matching ref: {ref}')
         return web.Response()
 
     column_name = 'inprog_column_id' if event.action == 'reopened' else 'done_column_id'
@@ -49,6 +56,7 @@ async def closed(event: github.EventType):
         for _, card in cards.iterrows():
             card_id = card['card_id']
             column_id = card[column_name]
+            logging.info(f'Moving card {card_id} to column {column_id}')
             await session.move_project_card(card_id=card_id, column_id=column_id)
 
     return web.Response()
