3218a8c0711375f373f5fe0f31bd6c1da3b673a3
Kevin Paul <kpaul@ucar.edu>
2020-08-07 10:19:07 -0600

    Make columns a dataframe for 2-way lookup

--- tests/test_projects.py
+++ tests/test_projects.py
@@ -8,7 +8,7 @@ from xdevbot import projects
 
 def test_build_config_frame():
     with open('tests/data/config.yaml') as f:
-        config_data = yaml.load(f)
+        config_data = yaml.safe_load(f)
     actual = projects.build_config_frame(config_data)
     expected = pd.read_csv('tests/data/config.csv')
     pd.testing.assert_frame_equal(actual, expected)
@@ -22,21 +22,26 @@ def test_build_cards_frame():
     pd.testing.assert_frame_equal(actual, expected)
 
 
-COLUMNS = {
-    'https://github.com/owner/repo/projects/1': {'New': 11, 'Done': 12, 'In Progress': 13},
-    'https://github.com/owner/repo/projects/2': {'New': 21, 'Done': 22, 'In Progress': 23},
+_data = {
+    'project_url': (
+        ['https://github.com/owner/repo/projects/1'] * 3
+        + ['https://github.com/owner/repo/projects/2'] * 3
+    ),
+    'column_name': ['New', 'Done', 'In Progress'] * 2,
+    'column_id': [11, 12, 13, 21, 22, 23],
 }
+COLUMNS = pd.DataFrame(data=_data)
 
 
-def test_build_columns_map_from_cards():
+def test_build_columns_frame_from_cards():
     with open('tests/data/cards.json') as f:
         cards_data = json.load(f)
-    actual = projects.build_columns_map(cards_data)
-    assert actual == COLUMNS
+    actual = projects.build_columns_frame(cards_data)
+    pd.testing.assert_frame_equal(actual, COLUMNS)
 
 
-def test_build_columns_map_from_columns():
+def test_build_columns_frame_from_columns():
     with open('tests/data/columns.json') as f:
         columns_data = json.load(f)
-    actual = projects.build_columns_map(columns_data)
-    assert actual == COLUMNS
+    actual = projects.build_columns_frame(columns_data)
+    pd.testing.assert_frame_equal(actual, COLUMNS)
