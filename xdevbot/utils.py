671c65e18a83c1aae3c363601c1084c493579372
Kevin Paul <kpaul@ucar.edu>
2020-08-10 15:43:05 -0600

    Try more complete usage of context manager

--- xdevbot/utils.py
+++ xdevbot/utils.py
@@ -47,21 +47,23 @@ async def log_rate_limits(
     timeout = ClientTimeout(total=timeout)
     async with ClientSession(headers=headers, timeout=timeout) as session:
         response = await session.get('https://api.github.com/rate_limit')
-    if response.status == 200:
-        rates = await response.json()
-        for k in category:
-            r_k = rates['resources'][k]
-            msg = f"{k.upper()} Rate Limits: {r_k['remaining']} remaining of {r_k['limit']} total"
-            logger.info(msg)
-    else:
-        logger.warning('Failed to retrieve rate limits')
+        if response.status == 200:
+            rates = await response.json()
+            for k in category:
+                r_k = rates['resources'][k]
+                msg = (
+                    f"{k.upper()} Rate Limits: {r_k['remaining']} remaining of {r_k['limit']} total"
+                )
+                logger.info(msg)
+        else:
+            logger.warning('Failed to retrieve rate limits')
 
 
 async def read_remote_yaml(url, timeout=60):
     timeout = ClientTimeout(total=timeout)
     async with ClientSession(timeout=timeout) as session:
         response = await session.get(url)
-    if response.status != 200:
-        raise RuntimeError(f'Failed to read config file: {response.status}')
-    text = await response.text()
+        if response.status != 200:
+            raise RuntimeError(f'Failed to read config file: {response.status}')
+        text = await response.text()
     return yaml.safe_load(text)
