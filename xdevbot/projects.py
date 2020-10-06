64e4f37b5f0e1d30ff6dbc638cd466fd7ba98d71
Kevin Paul <kpaul@ucar.edu>
2020-10-06 09:30:31 -0600

    Build complete cards frame

--- xdevbot/projects.py
+++ xdevbot/projects.py
@@ -25,15 +25,21 @@ def build_cards_frame(projects: dict) -> pd.DataFrame:
         'content_state': [],
         'creator': [],
         'column_id': [],
+        'column_name': [],
         'project_url': [],
         'new_column_id': [],
+        'easy_column_id': [],
+        'low_priority_column_id': [],
+        'high_priority_column_id': [],
+        'in_progress_column_id': [],
+        'stalled_column_id': [],
         'done_column_id': [],
-        'inprog_column_id': [],
     }
     for project in projects['data']['repository']['projects']['nodes']:
         url = project['url']
         for column in project['columns']['nodes']:
             column_id = column['databaseId']
+            column_name = column['name']
             for card in column['cards']['nodes']:
                 card_id = card['databaseId']
                 creator = card['creator']['login']
@@ -64,15 +70,24 @@ def build_cards_frame(projects: dict) -> pd.DataFrame:
                 data['content_state'].append(content_state)
                 data['creator'].append(creator)
                 data['column_id'].append(column_id)
+                data['column_name'].append(column_name)
                 data['project_url'].append(url)
 
                 df = columns[columns['project_url'] == url]
                 new_column_id = int(df[df['column_name'] == 'New']['column_id'])
                 data['new_column_id'].append(new_column_id)
+                easy_column_id = int(df[df['column_name'] == 'Easy']['column_id'])
+                data['easy_column_id'].append(easy_column_id)
+                low_priority_column_id = int(df[df['column_name'] == 'Low Priority']['column_id'])
+                data['low_priority_column_id'].append(low_priority_column_id)
+                high_priority_column_id = int(df[df['column_name'] == 'High Priority']['column_id'])
+                data['high_priority_column_id'].append(high_priority_column_id)
+                in_progress_column_id = int(df[df['column_name'] == 'In Progress']['column_id'])
+                data['in_progress_column_id'].append(in_progress_column_id)
+                stalled_column_id = int(df[df['column_name'] == 'Stalled']['column_id'])
+                data['stalled_column_id'].append(stalled_column_id)
                 done_column_id = int(df[df['column_name'] == 'Done']['column_id'])
                 data['done_column_id'].append(done_column_id)
-                inprog_column_id = int(df[df['column_name'] == 'In Progress']['column_id'])
-                data['inprog_column_id'].append(inprog_column_id)
     return pd.DataFrame(data=data)
 
 
