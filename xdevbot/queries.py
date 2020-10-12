8b2f57047c4092dda7362d35f028c27b490bdc50
Kevin Paul <kpaul@ucar.edu>
2020-10-12 12:02:11 -0600

    Query main board, too

--- xdevbot/queries.py
+++ xdevbot/queries.py
@@ -1,6 +1,6 @@
 GET_ALL_CARDS = """{
     repository(name: \"xdev\", owner: \"NCAR\") {
-        projects(first: 8, after: \"Y3Vyc29yOnYyOpHOACHFqQ==\") {
+        projects(first: 9, after: \"Y3Vyc29yOnYyOpHOACHFqQ==\") {
             nodes {
                 url
                 columns(first: 7) {
@@ -37,7 +37,7 @@ GET_ALL_CARDS = """{
 
 GET_COLUMNS = """{
     repository(name: \"xdev\", owner: \"NCAR\") {
-        projects(first: 8, after: \"Y3Vyc29yOnYyOpHOACHFqQ==\") {
+        projects(first: 9, after: \"Y3Vyc29yOnYyOpHOACHFqQ==\") {
             nodes {
                 url
                 columns(first: 7) {
